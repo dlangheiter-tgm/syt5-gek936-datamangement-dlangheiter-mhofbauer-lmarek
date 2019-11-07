63af1db2020a33409323092fa7c0996298fec064
mhofbauer-tgm <mhofbauer@student.tgm.ac.at>
2019-11-07 10:36:15 +0100

    Added temporary add and delete methods for list items

--- unicorn-webshop/src/index.js
+++ unicorn-webshop/src/index.js
@@ -9,6 +9,36 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-var db = new PouchDB('todos');
-var remoteCouch = false;
 serviceWorker.unregister();
+var PouchDB = require('pouchdb');
+var db = new PouchDB('my_db');
+var remoteCouch = false;
+
+
+/**
+ * Adds Item to list
+ * @param text
+ */
+function addShopping(text) {
+    var sitem = {
+        _id: new Date().toISOString(),
+        title: text,
+        completed: false
+    };
+    db.put(sitem, function callback(err, result) {
+        if (!err) {
+            console.log('Successfully added an item!');
+        }
+    });
+}
+
+/**
+ * Deletes specific item from the list
+ * @param shop
+ */
+function deleteButtonPressed(shop) {
+    db.remove(shop);
+}
+
+
+

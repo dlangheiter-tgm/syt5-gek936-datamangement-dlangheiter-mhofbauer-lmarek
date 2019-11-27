56e71fc63c8a3e0e8385e89b6a86568614fb9699
David Langheiter <david@langheiter.com>
2019-11-27 23:00:56 +0100

    Use props.create + reset input after add

--- unicorn-webshop/src/EntryList.js
+++ unicorn-webshop/src/EntryList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {IconButton, List, ListItem, ListItemSecondaryAction, TextField} from "@material-ui/core";
+import {IconButton, List, TextField} from "@material-ui/core";
 import {Entry} from "./Entry";
 import AddIcon from "@material-ui/icons/Add";
 
@@ -9,33 +9,38 @@ export class EntryList extends React.Component {
         super(props);
 
         this.state = {addTitle: ''};
-
-        this.setTitle = this.setTitle.bind(this);
     }
 
     render() {
         return (
             <div>
-            <List>
-                {this.props.list.map(e => <Entry
-                    key={e._id}
-                    entry={e}
-                    update={(v) => this.props.update(e, v)}
-                    delete={this.props.delete}
-                />)}
-            </List>
-            <br/>
-            <TextField value={this.state.addTitle} onChange={this.setTitle}/>
-            <IconButton onClick={() => this.props.create({title: this.state.addTitle})}>
-                <AddIcon/>
-            </IconButton>
+                <List>
+                    {this.props.list.map(e => <Entry
+                        key={e._id}
+                        entry={e}
+                        update={(v) => this.props.update(e, v)}
+                        delete={this.props.delete}
+                    />)}
+                </List>
+                <br/>
+                <TextField value={this.state.addTitle} onChange={this.setTitle}/>
+                <IconButton
+                    onClick={this.createItem}>
+                    <AddIcon/>
+                </IconButton>
             </div>
         );
     }
 
-    setTitle (e) {
-        console.log("SET", e.target.value);
+    setTitle = (e) => {
         this.setState({addTitle: e.target.value});
-    }
+    };
+
+    createItem = () => {
+        this.props.create({title: this.state.addTitle, commentary: this.state.addTitle});
+        this.setState({
+            addTitle: "",
+        });
+    };
 
 }
